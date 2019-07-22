91b9f069bee6d0b131a8efa130a74ec8f64efcc0
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 15:20:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,8 +7,10 @@ def find_max_value(array)
   length = array.length
   length.times{ |index|
     if array[index] > max
-      
+      max = array[index]
+    end
   }
+  max
 end
 
 def find_min_value(array)
